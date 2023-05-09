994f96252c7578aacf8d8dba6766ec5b63139a64
mkumachan <camysblossoms@gmail.com>
2023-05-09 23:46:19 +1000

    gogo

--- models/common.py
+++ models/common.py
@@ -3,7 +3,7 @@ import os
 db_password=os.getenv('DATABASE_PASSWORD')
 
 def sql_read(query,parameters=[]):
-    connection=psycopg2.connect(dbname="venue",user="postgres" ,host="127.0.0.1" ,password=db_password)
+    connection=psycopg2.connect(dbname=os.getenv('DBNAME'),user=os.getenv('DBUSER') ,host=os.getenv('DBHOST') ,password=db_password,port=os.getenv('DBPORT'))
     cursor=connection.cursor()
     cursor.execute(query,parameters)
     results = cursor.fetchall()
@@ -12,7 +12,7 @@ def sql_read(query,parameters=[]):
 
 
 def sql_read2(query):
-    connection=psycopg2.connect(dbname="venue",user="postgres" ,host="127.0.0.1" ,password=db_password)
+    connection=psycopg2.connect(dbname=os.getenv('DBNAME'),user=os.getenv('DBUSER') ,host=os.getenv('DBHOST') ,password=db_password,port=os.getenv('DBPORT'))
     cursor=connection.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
@@ -22,7 +22,7 @@ def sql_read2(query):
 
 
 def sql_write(query,parameters=[]):
-    connection=psycopg2.connect(dbname="venue",user="postgres" ,host="127.0.0.1" ,password=db_password)
+    connection=psycopg2.connect(dbname=os.getenv('DBNAME'),user=os.getenv('DBUSER') ,host=os.getenv('DBHOST') ,password=db_password,port=os.getenv('DBPORT'))
     cursor=connection.cursor()
     cursor.execute(query,parameters)
     connection.commit()
@@ -30,7 +30,7 @@ def sql_write(query,parameters=[]):
 
 
 def simple(query):
-    connection=psycopg2.connect(dbname="venue",user="postgres" ,host="127.0.0.1" ,password=db_password)
+    connection=psycopg2.connect(dbname=os.getenv('DBNAME'),user=os.getenv('DBUSER') ,host=os.getenv('DBHOST') ,password=db_password,port=os.getenv('DBPORT'))
     cursor=connection.cursor()
     cursor.execute(query)
     connection.commit()
