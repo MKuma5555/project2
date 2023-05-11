961171d77f5e1d913c059059a298fc49475be555
mkumachan <camysblossoms@gmail.com>
2023-05-11 19:36:32 +1000

    y

--- app.py
+++ app.py
@@ -301,4 +301,4 @@ def create_user():
      <p>Is this valid password? {isValidPassword}</p> """
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=os.getenv("PORT", default=5000))
